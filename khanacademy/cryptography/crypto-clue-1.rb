453d81d23aa5644a1299987f9e53a84762a316de
YaNing Zhang <zhangyaning1985@gmail.com>
2016-09-26 14:49:52 +0800

    Update

--- khanacademy/cryptography/crypto-clue-1.rb
+++ khanacademy/cryptography/crypto-clue-1.rb
@@ -10,3 +10,7 @@ irb(main):161:0> a.split('').map{|i| i.ord-7 < 97 ? 26 + i.ord-7 : i.ord-7  }.ma
 irb(main):172:0> b.split('').map{|i| i.ord-n < 97 ? 26 + i.ord-n : i.ord-n  }.map{|i| i.chr }.join
 => "startigrabbedeverythingicouldfindpleasereturnanyblueprintsforvaultandalarmdesignbasedonwhichbankyoudecideoniamsettingupsafehouseco"
 
+
+ze name blackout worried that our cipher is too weak on next message switch to vigenere cipher keyword is the hidden symbol of death in my favorite holbein end
+
+start i grabbed everything i could find please return any blueprints for vault and alarm design based on which bank you decide on i am setting up safe house co
