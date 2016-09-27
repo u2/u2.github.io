b25d9bd65e52be89560ad6f057685982536b0f19
u2 <zhangyaning1985@gmail.com>
2016-09-27 21:07:15 +0800

    clue2

---
+++ khanacademy/cryptography/crypto-clue-2.rb
@@ -0,0 +1,11 @@
+key: sskkuullll
+
+startwarningiheardreportofourbreakinonthenewsstillwaitingonalarmtests
+chedulesiwillreportbacktomorrowwithfinalplanforextrasecurityisuggestwebu
+rnourlettersafterreadingandswitchourletterstonumbersusingpolybiussquared
+ropmessageunderthebenchattrainstationend
+
+start warning i heard report of our break in on the news still waiting on alarm test
+schedules i will report back tomorrow with final plan for extra security i suggest we burn
+our letters after reading and switch our letters to numbers using polybius square
+drop message under the bench at train station end
